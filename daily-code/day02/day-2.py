efe86866806ebe73ecb518f3d7db8dd3085db18f
Ignacio <ignaescuvero@gmail.com>
2024-12-06 01:53:06 -0300

    Day 2

--- daily-code/day02/day-2.py
+++ daily-code/day02/day-2.py
@@ -8,8 +8,7 @@ import sys
 with open(sys.argv[1], 'r') as f:
     lines = f.readlines()
 
-def count_reports(line: str) -> bool:
-    reports = list(map(int, line.split(' ')))
+def count_reports(reports: list[int]) -> bool:
     if not all(1 <= c <= 3 for c in [abs(x1 - x2) for x1, x2 in zip(reports, reports[1:])]):
         return False
     if all(x1 < x2 for x1, x2 in zip(reports, reports[1:])):
@@ -18,13 +17,29 @@ def count_reports(line: str) -> bool:
         return True
     return False
 
+def comprob_reports(line: str) -> bool:
+    reports = list(map(int, line.split(' ')))
+    return count_reports(reports)
+
+def amend_reports(line: str) -> bool:
+    reports = list(map(int, line.split(' ')))
+    if count_reports(reports):
+        return True
+    for i in range(len(reports)):
+        if count_reports(reports[:i] + reports[i+1:]):
+            return True
+    return False
+
+
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
-# Result 407
-print(len([r for r in lines if count_reports(r)]))
+# Result is 407
+print(len([r for r in lines if comprob_reports(r)]))
 
 
 # ++++++++++++++++++++++++++++++++
 ## Part 2
 
+# Result is 459
+print(len([r for r in lines if amend_reports(r)]))
