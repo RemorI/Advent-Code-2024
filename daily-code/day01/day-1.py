85a24cac5b2c0c422691c89dd6a5c1a95635984d
Ignacio <ignaescuvero@gmail.com>
2024-12-06 00:38:32 -0300

    Day 1

--- daily-code/day01/day-1.py
+++ daily-code/day01/day-1.py
@@ -4,3 +4,18 @@
 # https://adventofcode.com/2024/day/1
 
 
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = [list(map(int, line.split())) for line in f.readlines()]
+
+list1, list2 = list(zip(*lines))
+
+first_list = list1
+#print(f'1st List: {first_list}')
+
+second_list = list2
+#print(f'2nd List: {second_list}')
+
+# Result is 1222801
+#print(sum(abs(x1 - x2) for x1, x2 in zip(sorted(list1), sorted(list2))))
