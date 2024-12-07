4e3a3e34143c0a7698f3ef7cfc8b740307aec3b0
Ignacio <ignaescuvero@gmail.com>
2024-12-07 14:24:40 -0300

    Day 3

--- daily-code/day03/day-3.py
+++ daily-code/day03/day-3.py
@@ -12,6 +12,20 @@ with open(sys.argv[1], 'r') as f:
 
 valid_mul = re.findall(r'mul\((\d{1,3}),(\d{1,3})\)', data)
 
+part2= 0
+enabled = True
+for inst in re.findall(r"mul\(\d{1,3},\d{1,3}\)|do\(\)|don't\(\)", data):
+    match inst:
+        case "do()":
+            enabled = True
+        case "don't()":
+            enabled = False
+        case _:
+            x, y = map(int, inst[4:-1].split(','))
+            if enabled:
+                part2 += x*y
+
+
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
@@ -22,4 +36,5 @@ print(sum(prod(map(int, val)) for val in valid_mul))
 # ++++++++++++++++++++++++++++++++
 ## Part 2
 
-# Result is
+# Result is 95846796
+print(part2)
