a286958008ea826c71049158556fcd68f7f0c7f9
Ignacio <ignaescuvero@gmail.com>
2024-12-10 16:52:00 -0300

    Day 6

---
+++ daily-code/day06/day-6.py
@@ -0,0 +1,20 @@
+# Day 6
+
+# Direct Link to adventofcode.com Day 6
+# https://adventofcode.com/2024/day/6
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = list(map(str.strip, f.readlines()))
+
+num_row = len(lines)
+num_cols = len(lines[0])
+
+
+
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is 
