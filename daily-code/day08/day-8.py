dbb4426443d0c9fb1e9e34f8fe19a0caf4753896
Ignacio <ignaescuvero@gmail.com>
2024-12-11 01:10:02 -0300

    Day 8

---
+++ daily-code/day08/day-8.py
@@ -0,0 +1,16 @@
+# Day 8
+
+# Direct Link to adventofcode.com Day 8
+# https://adventofcode.com/2024/day/8
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = list(map(str.strip, f.readlines()))
+
+
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
