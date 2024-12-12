65946b244f9cfd34e059bbca8a48953e27a6bb8d
Ignacio <ignaescuvero@gmail.com>
2024-12-11 22:33:11 -0300

    Day 10

---
+++ daily-code/day10/day-10.py
@@ -0,0 +1,16 @@
+# Day 10
+
+# Direct Link to adventofcode.com Day 10
+# https://adventofcode.com/2024/day/10
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    lines = list(map(str.strip, f.readlines()))
+
+
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
