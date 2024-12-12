b7065d9a6ac2e1b7d1043980ecea2847e7206dc9
Ignacio <ignaescuvero@gmail.com>
2024-12-11 22:32:51 -0300

    Day 9

--- daily-code/day09/day-9.py
+++ daily-code/day09/day-9.py
@@ -24,6 +24,34 @@ while True:
     disk[target] = disk.pop()
     i += 1
 
+isfile = True
+files = {}
+spaces = []
+ptr = 0
+for i, size in enumerate(data):
+    if isfile:
+        files[i//2] = (ptr, size)
+    else:
+        spaces.append((ptr, size))
+    isfile = not isfile
+    ptr += size
+
+for fid in reversed(files):
+    loc, file_size = files[fid]
+    space_id = 0
+    while space_id < len(spaces):
+        space_loc, space_size = spaces[space_id]
+        if space_loc > loc:
+            break
+        if space_size == file_size:
+            files[fid] = (space_loc, file_size)
+            spaces.pop(space_id)
+            break
+        if space_size >file_size:
+            files[fid] = (space_loc, file_size)
+            spaces[space_id] = (space_loc + file_size, space_size - file_size)
+            break
+        space_id += 1
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
@@ -34,4 +62,9 @@ print(sum(i*val for i, val in enumerate(disk)))
 # ++++++++++++++++++++++++++++++++
 ## Part 2
 
-# Result is
+# Result is 6421724645083
+count2 = 0
+for fid, (loc, size) in files.items():
+    for i in range(loc, loc + size):
+        count2 += fid * i
+print(count2)
