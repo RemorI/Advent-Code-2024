010f4d4a756b8dd6392edd3b2a8b7a6d06b156ff
Ignacio <ignaescuvero@gmail.com>
2024-12-14 16:14:55 -0300

    Day 11

--- daily-code/day11/day-11.py
+++ daily-code/day11/day-11.py
@@ -4,24 +4,45 @@
 # https://adventofcode.com/2024/day/11
 
 import sys
+from functools import cache
 
 with open(sys.argv[1], 'r') as f:
     stones = list(map(int, f.read().strip().split(" ")))
 
-for _ in range(25):
-    new_stones = []
-    for stone in stones:
-        if stone == 0:
-            new_stones.append(1)
-        elif len(str(stone)) % 2 == 0:
-            new_stones.append(int(str(stone)[len(str(stone))//2:]))
-            new_stones.append(int(str(stone)[:len(str(stone))//2]))
-        else:
-            new_stones.append(stone * 2024)
-    stones = new_stones
+# for _ in range(75):
+#     new_stones = []
+#     for stone in stones:
+#         if stone == 0:
+#             new_stones.append(1)
+#         elif len(str(stone)) % 2 == 0:
+#             new_stones.append(int(str(stone)[len(str(stone))//2:]))
+#             new_stones.append(int(str(stone)[:len(str(stone))//2]))
+#         else:
+#             new_stones.append(stone * 2024)
+#     stones = new_stones
+
+@cache
+def count_stones(val: int, blinks: int) -> int:
+    if blinks == 0:
+        return 1
+    if val == 0:
+        return count_stones(1, blinks-1)
+    str_val = str(val)
+    len_str_val = len(str_val)
+    if len_str_val % 2 == 0:
+        return count_stones(int(str_val[:len_str_val//2]), blinks-1) + count_stones(int(str_val[len_str_val//2:]), blinks-1)
+    return count_stones(val * 2024, blinks-1)
+
+
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
 # Result is 211306
-print(len(stones))
+print(sum(count_stones(s, 25) for s in stones))
+
+# ++++++++++++++++++++++++++++++++
+## Part 2
+
+# Result is 250783680217283
+print(sum(count_stones(s, 25) for s in stones))
