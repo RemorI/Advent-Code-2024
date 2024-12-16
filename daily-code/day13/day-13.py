8ce5fd2c1b2d0fa91e6669d3238edb994fca700b
Ignacio <ignaescuvero@gmail.com>
2024-12-16 17:18:50 -0300

    Day 13

---
+++ daily-code/day13/day-13.py
@@ -0,0 +1,15 @@
+# Day 12
+
+# Direct Link to adventofcode.com Day 12
+# https://adventofcode.com/2024/day/12
+
+import sys
+
+with open(sys.argv[1], 'r') as f:
+    grid = list(map(str.strip, f.readlines()))
+
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
