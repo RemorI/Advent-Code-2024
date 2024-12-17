c94ac6b2d76593375aa8956ff9488374b502a43c
Ignacio <ignaescuvero@gmail.com>
2024-12-17 03:29:16 -0300

    Day 13

--- daily-code/day13/day-13.py
+++ daily-code/day13/day-13.py
@@ -1,12 +1,14 @@
-# Day 12
+# Day 13
 
-# Direct Link to adventofcode.com Day 12
-# https://adventofcode.com/2024/day/12
+# Direct Link to adventofcode.com Day 13
+# https://adventofcode.com/2024/day/13
 
 import sys
 
 with open(sys.argv[1], 'r') as f:
     grid = list(map(str.strip, f.readlines()))
+
+
 
 
 # ++++++++++++++++++++++++++++++++
