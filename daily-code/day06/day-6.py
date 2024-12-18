c9d25e160e57fe8283a59340054b05684dd202bb
Ignacio <ignaescuvero@gmail.com>
2024-12-18 00:34:25 -0300

    [Fix] Day 6

--- daily-code/day06/day-6.py
+++ daily-code/day06/day-6.py
@@ -11,10 +11,37 @@ with open(sys.argv[1], 'r') as f:
 num_row = len(lines)
 num_cols = len(lines[0])
 
+def starter():
+    for r, row in enumerate(lines):
+        for c, val in enumerate(row):
+            if val == "^":
+                return (r, c)
+
+r, c = starter()
+dr, dc = -1, 0
+visited = set()
+
+while True:
+    visited.add((r, c))
+    if not (0 <= r + dr < num_row and 0 <= c + dc < num_cols):
+        break
+    if lines[r+dr][c+dc] == "#":
+        dc, dr = -dr, dc
+    else:
+        r += dr
+        c += dc
+
+
 
 
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
-# Result is 
+# Result is 4758
+print(len(visited))
+
+# ++++++++++++++++++++++++++++++++
+## Part 2
+
+# Result is
