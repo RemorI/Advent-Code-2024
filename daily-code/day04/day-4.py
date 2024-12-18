40dd00e09105108cbec8a15e863735c6f0b11079
Ignacio <ignaescuvero@gmail.com>
2024-12-18 00:13:47 -0300

    [Fix] Day 4

--- daily-code/day04/day-4.py
+++ daily-code/day04/day-4.py
@@ -4,41 +4,51 @@
 # https://adventofcode.com/2024/day/4
 
 import sys
-import numpy as np
 from collections import defaultdict
-import pandas as pd
 
 with open(sys.argv[1], 'r') as f:
     lines = f.readlines()
 
-prueba = np.array(lines)
-
-result = []
 char_map = defaultdict(set)
-for i in range(len(prueba)):
-    result.append(list(prueba[i]))
+for r, row in enumerate(lines):
+    for c, val in enumerate(row):
+        char_map[val].add((r,c))
 
-df = pd.DataFrame(result)
-fila, columna = df.shape
+count1 = 0
+for r, c in char_map["X"]:
+    for dr, dc in [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]:
+        for i, char in enumerate("MAS", 1):
+            if (r + (dr * i), c + (dc * i)) not in char_map[char]:
+                break
+        else:
+            count1 += 1
 
-coordenadas = [(x, y) for x, resu in enumerate(result) for y, char in enumerate(resu) if char == 'X']
-compro = [(-1,-1),(-1,0),(-1,1),(0,-1),(0,1),(1,-1),(1,0),(1,1)]
+upleft = lambda r, c: (r - 1, c - 1)
+upright = lambda r, c: (r - 1, c + 1)
+downleft = lambda r, c: (r + 1, c - 1)
+downright = lambda r, c: (r + 1, c + 1)
 
-part1=0
-for x,y in coordenadas:
-    for cr, cp in compro:
-        xM,yM=x+cr, y+cp
-        if result[xM][yM] == 'M':
-            coor_M = (xM,yM)
-            xA, yA= xM+cr, yM+cp
-            if result[xA][yA] == 'A':
-                coor_A = (xA,yA)
-                xS, yS = xA+cr, yA+cp
-                if result[xS][yS] == 'S':
-                    part1 +=1
+count2 = 0
+for r, c in char_map["A"]:
+    if upleft(r, c) in char_map["M"]:
+        if downleft(r, c) in char_map["M"] and upright(r, c) in char_map["S"] and downright(r, c) in char_map["S"]:
+            count2 += 1
+        if upright(r, c) in char_map["M"] and downleft(r, c) in char_map["S"] and downright(r, c) in char_map["S"]:
+            count2 += 1
+    if downright(r, c) in char_map["M"]:
+        if downleft(r, c) in char_map["M"] and upright(r, c) in char_map["S"] and upleft(r, c) in char_map["S"]:
+            count2 += 1
+        if upright(r, c) in char_map["M"] and downleft(r, c) in char_map["S"] and upleft(r, c) in char_map["S"]:
+            count2 += 1
 
-print(part1)
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
-# Result is
+# Result is 2578
+print(count1)
+
+# ++++++++++++++++++++++++++++++++
+## Part 2
+
+# Result is 1972
+print(count2)
