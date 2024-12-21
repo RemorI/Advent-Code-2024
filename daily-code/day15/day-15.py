b207f9b4a8f2dc50dc6a9983445dd29eb9c73066
Ignacio <ignaescuvero@gmail.com>
2024-12-21 03:28:41 -0300

    Day 15

--- daily-code/day15/day-15.py
+++ daily-code/day15/day-15.py
@@ -9,16 +9,40 @@ with open(sys.argv[1], 'r') as f:
     grid, movement = f.read().split('\n\n')
 
 movement = list(map(str, movement))
-grid = list(map(str, grid.split("\n")))
+grid = [list(map(str, line)) for line in grid.splitlines()]
 num_row = len(grid)
 num_cols = len(grid[0])
-left, right, up, down = "<", ">", "^", "v"
+#up, down, left, right = (-1, 0), (1, 0), (0, -1), (0, 1)
 
 
-print(num_row, num_cols)
 
+for r, row in enumerate(grid):
+    for c, col in enumerate(grid[0]):
+        if grid[r][c] == "@":
+            rro, cro = r, c
+        for move in movement:
+            if "^":
+                if grid[r-1][c] == "#": #
+                    continue
+                if not grid[r-1][c] == "O" and not grid[r-1][c] == "#": #
+                    rro, cro = (r-1, c)
+                    grid[rro][cro] = "@"
+                    grid[r][c] = "."
+                if  grid[r-1][c] == "O": #
+                    if grid[r-2][c] == "#": #
+                        continue
+                    else:
+                        grid[rro][cro] = "@"
+                        grid[r][c] = "."
+                        count = -2#
+                        while grid[r+count][c] == "O" and not grid[r+count-1][c] == "#":
+                            grid[r+count-1][c] = "O"
+                            count += 1#
 
-print(num_row, num_cols)
+# O O @ .
+
+print(grid)
+print(len(movement))
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
