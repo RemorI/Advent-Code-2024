71fe0d39e2f43877ad7e3e6a9955f50638e5792c
Ignacio <ignaescuvero@gmail.com>
2024-12-29 14:38:15 -0300

    Day 17

---
+++ daily-code/day17/day-17.py
@@ -0,0 +1,17 @@
+# Day 16
+
+# Direct Link to adventofcode.com Day 16
+# https://adventofcode.com/2024/day/16
+
+import sys
+import re
+
+with open(sys.argv[1], 'r') as f:
+    register, program = list(map(str, f.split("\n\n")))
+
+print(register, program)
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
