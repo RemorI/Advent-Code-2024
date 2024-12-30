130095fc402c5ce8a54ac9c3e5d343951a6ac11f
Ignacio <ignaescuvero@gmail.com>
2024-12-30 15:11:54 -0300

    Day 18 Part 2

--- daily-code/day18/day-18.py
+++ daily-code/day18/day-18.py
@@ -20,23 +20,38 @@ with open(sys.argv[1], 'r') as f:
 
 blocks = [tuple(map(int, line.split(',')))[::-1] for line in lines]
 
-queue = deque([(0, 0, 0)])
-seen = set((0, 0))
-while queue:
-    r, c, d = queue.popleft()
-    if (r, c) in seen:
-        continue
-    seen.add((r, c))
-    if r == num_rows -1 and c == num_cols -1:
-        break
-    for dr, dc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
-        nr, nc = r + dr, c+ dc
-        if 0 <=nr < num_rows and 0 <= nc < num_cols and (nr, nc) not in blocks[:part1_num]:
-            queue.append((nr, nc, d + 1))
+def count_steps(num_blocks: int) -> int:
+    queue = deque([(0, 0, 0)])
+    seen = set((0, 0))
+    while queue:
+        r, c, d = queue.popleft()
+        if (r, c) in seen:
+            continue
+        seen.add((r, c))
+        if r == num_rows -1 and c == num_cols -1:
+            return d
+        for dr, dc in [(-1, 0), (0, -1), (1, 0), (0, 1)]:
+            nr, nc = r + dr, c+ dc
+            if 0 <=nr < num_rows and 0 <= nc < num_cols and (nr, nc) not in blocks[:num_blocks]:
+                queue.append((nr, nc, d + 1))
+    return -1
 
+lo, hi = 0, len(blocks) - 1
+while lo < hi:
+    mid = (lo + hi) // 2
+    if count_steps(mid + 1) != -1:
+        lo = mid + 1
+    else:
+        hi = mid
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
 # Result is 282
-print(d)
+print(count_steps(part1_num))
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
+print(",".join(str(x) for x in blocks[lo][::-1]))
