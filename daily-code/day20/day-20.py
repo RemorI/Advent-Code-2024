3ccec4c06836e113062b099890118bd99cb9e030
Ignacio <ignaescuvero@gmail.com>
2025-01-01 17:31:09 -0300

    Day 20

---
+++ daily-code/day20/day-20.py
@@ -0,0 +1,17 @@
+# Day 19
+
+# Direct Link to adventofcode.com Day 19
+# https://adventofcode.com/2024/day/19
+
+
+import sys
+
+with open(sys.argv[1], "r") as f:
+    options = f.readline().strip().split(", ")
+
+
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
