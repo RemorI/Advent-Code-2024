5e6e54c66df403f08eb3dc4c96cbf8c38cfc70d6
Ignacio <ignaescuvero@gmail.com>
2025-01-02 17:03:27 -0300

    Day 20 Part 2

--- daily-code/day20/day-20.py
+++ daily-code/day20/day-20.py
@@ -13,7 +13,6 @@ with open(sys.argv[1], "r") as f:
 
 num_rows = len(grid)
 num_cols = len(grid[0])
-
 for r, row in enumerate(grid):
     for c, val in enumerate(row):
         if val == "S":
@@ -22,28 +21,50 @@ for r, row in enumerate(grid):
         continue
     break
 
+def count_shortcuts(max_cheat: int, target_savings: int) -> defaultdict:
+    shortcuts = defaultdict(int)
+    for r in range(num_rows):
+        for c in range(num_cols):
+            if (r, c) not in dists: continue
+            for len_cheat in range(2, max_cheat + 1):
+                for dr in range(len_cheat + 1):
+                    dc = len_cheat - dr
+                    for r2, c2 in set(
+                        [
+                            (r + dr, c + dc),
+                            (r + dr, c - dc),
+                            (r - dr, c + dc),
+                            (r - dr, c - dc),
+                        ]
+                    ):
+                        if (r2, c2) not in dists: continue
+                        time_saved = dists[(r2, c2)] - dists[(r, c)] - len_cheat
+                        if time_saved >= target_savings:
+                            shortcuts[time_saved] += 1
+    return shortcuts
+
 queue = deque([(r, c, 0)])
 dists = {}
 while queue:
     r, c, d = queue.popleft()
     if (r, c) is dists: continue
     dists[(r, c)] = d
-    for nr, nc in [(r, c -1), (r, c +1), (r-1, c), (r+1, c)]:
+    for nr, nc in [(r, c - 1), (r, c + 1), (r - 1, c), (r + 1, c)]:
         if grid[nr][nc] != "#" and (nr, nc) not in dists:
-            queue.appendleft((nr, nc, d+1))
+            queue.appendleft((nr, nc, d + 1))
 
-shortcuts = defaultdict(int)
-for r in range(num_rows):
-    for c in range(num_cols):
-        for r2, c2 in [(r-2, c), (r+2, c), (r, c-2), (r, c+2), (r+1, c+1), (r+1, c-1), (r-1, c+1), (r-1, c-1)]:
-            if (r, c) not in dists or (r2, c2) not in dists: continue
-            time_saved = dists[(r2, c2)] - dists[(r, c)] - 2
-            if time_saved >= target_savings:
-                shortcuts[time_saved] += 1
+
 
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
 # Result is 1197
-print(sum(c for c in shortcuts.values()))
+print(sum(c for c in count_shortcuts(2, target_savings).values()))
+# ++++++++++++++++++++++++++++++++
+## Part 2
+
+# Result is 944910
+if target_savings == 1:
+    target_savings = 50
+print(sum(c for c in count_shortcuts(20, target_savings).values()))
