df5d22098d7c8b800cdffbd1e8fea93576d3ac58
Ignacio <ignaescuvero@gmail.com>
2025-01-02 17:27:58 -0300

    Day 21

---
+++ daily-code/day21/day-21.py
@@ -0,0 +1,14 @@
+# Day 21
+
+# Direct Link to adventofcode.com Day 21
+# https://adventofcode.com/2024/day/21
+
+import sys
+
+with open(sys.argv[1], "r") as f:
+    code = list(map(str.strip, f.readlines()))
+
+# ++++++++++++++++++++++++++++++++
+## Part 1
+
+# Result is
