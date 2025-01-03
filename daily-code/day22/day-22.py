c3244491a0827f969c8fdc264562a2a2b2bdf5bc
Ignacio <ignaescuvero@gmail.com>
2025-01-03 16:39:10 -0300

    Day 22 Part 1

--- daily-code/day22/day-22.py
+++ daily-code/day22/day-22.py
@@ -6,10 +6,23 @@
 import sys
 
 with open(sys.argv[1], "r") as f:
-    lines = list(map(str.strip, f.readlines()))
+    numbers = list(map(int, f.readlines()))
 
+
+def next_num(x: int) -> int:
+    x ^= (x * 64) % 16777216
+    x ^= (x // 32) % 16777216
+    x ^= (x * 2048) % 16777216
+    return x
+
+count1 = 0
+for num in numbers:
+    for _ in range(2000):
+        num = next_num(num)
+    count1 += num
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
-# Result is
+# Result is 15613157363
+print(count1)
