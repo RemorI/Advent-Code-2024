294e2302844996e10ae579f079fbb7a27c259dd9
Ignacio <ignaescuvero@gmail.com>
2025-01-04 14:41:34 -0300

    Day 23 Part 2

--- daily-code/day23/day-23.py
+++ daily-code/day23/day-23.py
@@ -9,6 +9,17 @@ from collections import defaultdict
 with open(sys.argv[1], "r") as f:
     pairs = [l.strip().split('-') for l in f.readlines()]
 
+passwords = set()
+def build_set(conn: str, group: set[str]) -> None:
+    password = ','.join(sorted(group))
+    if password in passwords: return
+    passwords.add(password)
+    for neighbor in conns[conn]:
+        if neighbor in group: continue
+        if any(neighbor not in conns[node] for node in group): continue
+        build_set(neighbor, {*group, neighbor})
+
+
 conns = defaultdict(set)
 for a, b in pairs:
     conns[a].add(b)
@@ -21,10 +32,17 @@ for conn in conns:
             if conn in conns[nn]:
                 triples.add(tuple(sorted([conn, neighbor, nn])))
 
-
+for conn in conns:
+    build_set(conn, {conn})
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
 # Result is
 print(len([t for t in triples if any(x.startswith("t") for x in t)]))
+
+# ++++++++++++++++++++++++++++++++
+## Part 2
+
+# Result is
+print(max(passwords, key=len))
