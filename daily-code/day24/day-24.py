7c0f7108c01a47563a6dcbcbd1620a9715689c73
Ignacio <ignaescuvero@gmail.com>
2025-01-07 13:06:06 -0300

    [Test] Day 24

--- daily-code/day24/day-24.py
+++ daily-code/day24/day-24.py
@@ -5,6 +5,20 @@
 
 import sys
 import re
+from dataclasses import dataclass
+
+@dataclass
+class Connection:
+    in1: str
+    in2: str
+    op: str
+    out: str
+
+operations = {
+    "OR": lambda x1, x2: x1 | x2,
+    "AND": lambda x1, x2: x1 & x2,
+    "XOR": lambda x1, x2: x1 ^ x2
+}
 
 with open(sys.argv[1], "r") as f:
     bits, connections = f.read().split("\n\n")
@@ -15,18 +29,21 @@ map_str = connections.splitlines()
 wire_map = {}
 for wire in map_str:
     in1, op, in2, _, out = wire.strip().split(" ")
-    wire_map[out]
+    wire_map[out] = Connection(in1, in2, op, out)
 
-print(init, map_str)
+def run_wire(w:str):
+    if w in init:
+        return init[w]
+    conn = wire_map[w]
+    return operations[conn.op](run_wire(conn.in1), run_wire(conn.in2))
 
-
-operations = {
-    "OR": lambda x1, x2: x1 | x2,
-    "AND": lambda x1, x2: x1 & x2,
-    "XOR": lambda x1, x2: x1 ^ x2
-}
+result = [
+    run_wire(w)
+    for w in sorted([w for w in wire_map if w.startswith("z")], reverse=True)
+]
 
 # ++++++++++++++++++++++++++++++++
 ## Part 1
 
 # Result is
+print(int("".join(map(str, result)), 2))
